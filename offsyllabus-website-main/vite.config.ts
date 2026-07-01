73456f88dec3f228c2cbb1d4cdc55058d1c6aab6
Abhishek Zadokar <zadokarabhishek@gmail.com>
2026-07-01 09:21:19 +0530

    Switch to Vercel deployment

--- offsyllabus-website-main/vite.config.ts
+++ offsyllabus-website-main/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
@@ -9,7 +8,6 @@ export default defineConfig({
   plugins: [
     viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tanstackStart(),
     viteReact(),
   ],
