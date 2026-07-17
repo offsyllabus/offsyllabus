50b2a9023e4f563b141a3adea11a8561da969e75
Abhishek Zadokar <zadokarabhishek@gmail.com>
2026-07-17 13:15:19 +0530

    Add Google Analytics tag

--- offsyllabus-website-main/src/routes/__root.tsx
+++ offsyllabus-website-main/src/routes/__root.tsx
@@ -33,6 +33,20 @@ export const Route = createRootRoute({
         href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Bricolage+Grotesque:opsz,wght@12..60,400;12..60,500;12..60,600;12..60,700;12..60,800&display=swap',
       },
     ],
+    scripts: [
+      {
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-C4DNZ01ZF1',
+        async: true,
+      },
+      {
+        children: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-C4DNZ01ZF1');
+        `,
+      },
+    ],
   }),
   component: RootComponent,
 })
